adc441f91192686513f741eb58bc5f89c2b86c89
niuyulong99 <1461893122@qq.com>
2024-03-14 12:26:59 +0800

    添加新的视图函数views.py

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from  django.http  import  HttpRspose
-
+from  django.http  import  redis
 
 def  index(reques):
     reture  HttpResponse
+
+
+def  index(reques):
+    reture   redis
+~                       
